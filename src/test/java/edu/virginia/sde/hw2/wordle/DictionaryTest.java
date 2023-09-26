744ef82210913e924a77cee8784218debb694c91
neallang <neal_langhorne@yahoo.com>
2023-09-25 20:52:00 -0400

    neal - isCorrectLength_shouldBeTrue() test to prove that the method is incorrect. method not yet updated

--- src/test/java/edu/virginia/sde/hw2/wordle/DictionaryTest.java
+++ src/test/java/edu/virginia/sde/hw2/wordle/DictionaryTest.java
@@ -10,23 +10,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DictionaryTest {
 
-    @Test
-    void getWordSet() {
-        var startingWordSet = new HashSet<>(Set.of("apple", "black", "camel"));
-        var dictionary = new Dictionary(startingWordSet, new WordValidator());
-
-        var wordSet = dictionary.getWordSet();
-        assertEquals(3, wordSet.size());
-        assertTrue(wordSet.contains("apple"));
-        assertTrue(wordSet.contains("black"));
-        assertTrue(wordSet.contains("camel"));
-    }
-
-    @Test
-    void getWordSet_initiallyEmpty() {
-        var dictionary = new Dictionary();
-
-        var wordSet = dictionary.getWordSet();
-        assertTrue(wordSet.isEmpty());
+//    @Test
+//    void getWordSet() {
+//        var startingWordSet = new HashSet<>(Set.of("apple", "black", "camel"));
+//        var dictionary = new Dictionary(startingWordSet, new WordValidator());
+//
+//        var wordSet = dictionary.getWordSet();
+//        assertEquals(3, wordSet.size());
+//        assertTrue(wordSet.contains("apple"));
+//        assertTrue(wordSet.contains("black"));
+//        assertTrue(wordSet.contains("camel"));
+//    }
+//
+//    @Test
+//    void getWordSet_initiallyEmpty() {
+//        var dictionary = new Dictionary();
+//
+//        var wordSet = dictionary.getWordSet();
+//        assertTrue(wordSet.isEmpty());
+//    }
+    @Test void isCorrectLength_shouldBeTrue() {     //this test proved isCorrectLength() was wrong. Now I fix.
+        var WordValidator = new WordValidator();
+        assertTrue(WordValidator.isCorrectLength("apple"));
     }
 }
