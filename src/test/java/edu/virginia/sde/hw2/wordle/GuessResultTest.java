36bebc092b2b73e030a003ac8e2ac1979d069190
dknow17 <msv7hq@virginia.edu>
2023-09-29 19:57:58 -0400

    part 2 final

--- src/test/java/edu/virginia/sde/hw2/wordle/GuessResultTest.java
+++ src/test/java/edu/virginia/sde/hw2/wordle/GuessResultTest.java
@@ -10,89 +10,89 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GuessResultTest {
 
-//    @Test
-//    void isCorrect_expectTrue(){
-//        var guess = new GuessResult("false","false");
-//        assertTrue(guess.isCorrect());
-//    }
-//
-//    @Test
-//    void getLetterResults_allRight(){
-//        var guess = new GuessResult("false","false");
-//        var expectedResult = new LetterResult[] {LetterResult.GREEN, LetterResult.GREEN,
-//        LetterResult.GREEN, LetterResult.GREEN, LetterResult.GREEN};
-//        for (int i = 0; i <5; i++) {
-//            assertEquals(expectedResult[i], guess.getLetterResults()[i]);
-//        }
-//    }
-//        @Test
-//    void getLetterResults_allWrong(){
-//        var guess = new GuessResult("false","zzzzz");
-//        var expectedResult = new LetterResult[] {LetterResult.GRAY, LetterResult.GRAY,
-//        LetterResult.GRAY, LetterResult.GRAY, LetterResult.GRAY};
-//        for (int i = 0; i <5; i++) {
-//            assertEquals(expectedResult[i], guess.getLetterResults()[i]);
-//        }
-//    }
-//    @Test
-//    void getLetterResults_oneYellow(){
-//        var guess = new GuessResult("zfzzz","false");
-//        var expectedResult = new LetterResult[] {LetterResult.GRAY, LetterResult.YELLOW,
-//        LetterResult.GRAY, LetterResult.GRAY, LetterResult.GRAY};
-//        for (int i = 0; i <5; i++) {
-//            assertEquals(expectedResult[i], guess.getLetterResults()[i]);
-//        }
-//    }
-//    @Test
-//    void getLetterResults_multipleYellow(){
-//        var guess = new GuessResult("pxxpx","apple");
-//        var expectedResult = new LetterResult[] {LetterResult.YELLOW, LetterResult.GRAY,
-//                LetterResult.GRAY, LetterResult.YELLOW, LetterResult.GRAY};
-//        for (int i = 0; i <5; i++) {
-//            assertEquals(expectedResult[i], guess.getLetterResults()[i]);
-//        }
-//}
-//    @Test
-//    void getLetterResults_tooManyYellow(){
-//        var guess = new GuessResult("pxxpp","apple");
-//        var expectedResult = new LetterResult[] {LetterResult.YELLOW, LetterResult.GRAY,
-//                LetterResult.GRAY, LetterResult.YELLOW, LetterResult.GRAY};
-//        for (int i = 0; i <5; i++) {
-//            assertEquals(expectedResult[i], guess.getLetterResults()[i]);
-//        }
-//}
-//    @Test
-//    void getLetterResults_yellowAndGreen(){
-//        var guess = new GuessResult("cheer","close");
-//        var expectedResult = new LetterResult[] {LetterResult.GREEN, LetterResult.GRAY,
-//                LetterResult.YELLOW, LetterResult.GRAY, LetterResult.GRAY};
-//        for (int i = 0; i <5; i++) {
-//            assertEquals(expectedResult[i], guess.getLetterResults()[i]);
-//        }
-//    }
-//    @Test
-//    void getLetterResults_greenAndGray(){
-//        var guess = new GuessResult("cocks","close");
-//        var expectedResult = new LetterResult[] {LetterResult.GREEN, LetterResult.YELLOW,
-//                LetterResult.GRAY, LetterResult.GRAY, LetterResult.YELLOW};
-//        for (int i = 0; i <5; i++) {
-//            assertEquals(expectedResult[i], guess.getLetterResults()[i]);
-//        }
-//    }
-//    @Test
-//    void getLetterResults_grayBeforeGreen(){
-//        var guess = new GuessResult("leave","close");
-//        var expectedResult = new LetterResult[] {LetterResult.YELLOW, LetterResult.GRAY,
-//                LetterResult.GRAY, LetterResult.GRAY, LetterResult.GREEN};
-//        for (int i = 0; i <5; i++) {
-//            assertEquals(expectedResult[i], guess.getLetterResults()[i]);
-//        }
-//    }
-//@Test
-//void constructor_tooLong(){
-//    var guess = new GuessResult("meme54","clos5");
-//}
     @Test
+    void isCorrect_expectTrue(){
+        var guess = new GuessResult("false","false");
+        assertTrue(guess.isCorrect());
+    }
+
+    @Test
+    void getLetterResults_allRight(){
+        var guess = new GuessResult("false","false");
+        var expectedResult = new LetterResult[] {LetterResult.GREEN, LetterResult.GREEN,
+        LetterResult.GREEN, LetterResult.GREEN, LetterResult.GREEN};
+        for (int i = 0; i <5; i++) {
+            assertEquals(expectedResult[i], guess.getLetterResults()[i]);
+        }
+    }
+        @Test
+    void getLetterResults_allWrong(){
+        var guess = new GuessResult("false","zzzzz");
+        var expectedResult = new LetterResult[] {LetterResult.GRAY, LetterResult.GRAY,
+        LetterResult.GRAY, LetterResult.GRAY, LetterResult.GRAY};
+        for (int i = 0; i <5; i++) {
+            assertEquals(expectedResult[i], guess.getLetterResults()[i]);
+        }
+    }
+    @Test
+    void getLetterResults_oneYellow(){
+        var guess = new GuessResult("zfzzz","false");
+        var expectedResult = new LetterResult[] {LetterResult.GRAY, LetterResult.YELLOW,
+        LetterResult.GRAY, LetterResult.GRAY, LetterResult.GRAY};
+        for (int i = 0; i <5; i++) {
+            assertEquals(expectedResult[i], guess.getLetterResults()[i]);
+        }
+    }
+    @Test
+    void getLetterResults_multipleYellow(){
+        var guess = new GuessResult("pxxpx","apple");
+        var expectedResult = new LetterResult[] {LetterResult.YELLOW, LetterResult.GRAY,
+                LetterResult.GRAY, LetterResult.YELLOW, LetterResult.GRAY};
+        for (int i = 0; i <5; i++) {
+            assertEquals(expectedResult[i], guess.getLetterResults()[i]);
+        }
+}
+    @Test
+    void getLetterResults_tooManyYellow(){
+        var guess = new GuessResult("pxxpp","apple");
+        var expectedResult = new LetterResult[] {LetterResult.YELLOW, LetterResult.GRAY,
+                LetterResult.GRAY, LetterResult.YELLOW, LetterResult.GRAY};
+        for (int i = 0; i <5; i++) {
+            assertEquals(expectedResult[i], guess.getLetterResults()[i]);
+        }
+}
+    @Test
+    void getLetterResults_yellowAndGreen(){
+        var guess = new GuessResult("cheer","close");
+        var expectedResult = new LetterResult[] {LetterResult.GREEN, LetterResult.GRAY,
+                LetterResult.YELLOW, LetterResult.GRAY, LetterResult.GRAY};
+        for (int i = 0; i <5; i++) {
+            assertEquals(expectedResult[i], guess.getLetterResults()[i]);
+        }
+    }
+    @Test
+    void getLetterResults_greenAndGray(){
+        var guess = new GuessResult("cocks","close");
+        var expectedResult = new LetterResult[] {LetterResult.GREEN, LetterResult.YELLOW,
+                LetterResult.GRAY, LetterResult.GRAY, LetterResult.YELLOW};
+        for (int i = 0; i <5; i++) {
+            assertEquals(expectedResult[i], guess.getLetterResults()[i]);
+        }
+    }
+    @Test
+    void getLetterResults_grayBeforeGreen(){
+        var guess = new GuessResult("leave","close");
+        var expectedResult = new LetterResult[] {LetterResult.YELLOW, LetterResult.GRAY,
+                LetterResult.GRAY, LetterResult.GRAY, LetterResult.GREEN};
+        for (int i = 0; i <5; i++) {
+            assertEquals(expectedResult[i], guess.getLetterResults()[i]);
+        }
+    }
+    @Test                       //SHOULD ALWAYS FAIL!
+    void constructor_tooLong(){
+        var guess = new GuessResult("meme54","clos5");
+}
+    @Test                       //SHOULD ALWAYS FAIL!
     void constructor_invalidInputs(){
         var guess = new GuessResult("meme5","clos5");
     }
