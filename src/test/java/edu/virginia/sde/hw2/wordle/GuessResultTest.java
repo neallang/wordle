e1ab9ba2d6fc6f0a8675291363764243ac712ea8
dknow17 <msv7hq@virginia.edu>
2023-09-29 18:41:14 -0400

    getLetterResults_multipleYellow(): test to see if multiple correct letters are in the guess, but not in the right place; failed

--- src/test/java/edu/virginia/sde/hw2/wordle/GuessResultTest.java
+++ src/test/java/edu/virginia/sde/hw2/wordle/GuessResultTest.java
@@ -43,15 +43,24 @@ class GuessResultTest {
 //            assertEquals(expectedResult[i], guess.getLetterResults()[i]);
 //        }
 //    }
-            @Test
-    void getLetterResults_oneYellow(){
-        var guess = new GuessResult("zfzzz","false");
-        var expectedResult = new LetterResult[] {LetterResult.GRAY, LetterResult.YELLOW,
-        LetterResult.GRAY, LetterResult.GRAY, LetterResult.GRAY};
+//    @Test
+//    void getLetterResults_oneYellow(){
+//        var guess = new GuessResult("zfzzz","false");
+//        var expectedResult = new LetterResult[] {LetterResult.GRAY, LetterResult.YELLOW,
+//        LetterResult.GRAY, LetterResult.GRAY, LetterResult.GRAY};
+//        for (int i = 0; i <5; i++) {
+//            assertEquals(expectedResult[i], guess.getLetterResults()[i]);
+//        }
+//    }
+    @Test
+    void getLetterResults_multipleYellow(){
+        var guess = new GuessResult("pxxpx","apple");
+        var expectedResult = new LetterResult[] {LetterResult.YELLOW, LetterResult.GRAY,
+                LetterResult.GRAY, LetterResult.YELLOW, LetterResult.GRAY};
         for (int i = 0; i <5; i++) {
             assertEquals(expectedResult[i], guess.getLetterResults()[i]);
         }
-    }
+}
 
 
 
